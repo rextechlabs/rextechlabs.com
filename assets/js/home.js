0f454d093d56fed83062a8b480309124e14c452d
frederikstroem <git@katofln.com>
2023-12-20 19:56:53 +0100

    Website rework

---
+++ assets/js/home.js
@@ -0,0 +1,41 @@
+const phrases = ["Advanced security systems.", "More info coming soon..."];
+let currentPhraseIndex = 0;
+let currentCharIndex = 0;
+let isDeleting = false;
+const typingSpeed = 110; // Speed when typing
+const deletingSpeed = 70; // Speed when deleting
+const delayAfterPhrase = 1500; // Delay after finishing a phrase
+const delayAfterDeleting = 750; // Delay after deleting before starting next phrase
+const animatedText = document.getElementById('animatedText');
+
+function type() {
+    const fullText = phrases[currentPhraseIndex];
+    if (isDeleting) {
+        // Deleting characters
+        currentCharIndex--;
+    } else {
+        // Typing characters
+        currentCharIndex++;
+    }
+
+    animatedText.textContent = fullText.substring(0, currentCharIndex);
+
+    let typingDelay = isDeleting ? deletingSpeed : typingSpeed;
+
+    if (!isDeleting && currentCharIndex === fullText.length) {
+        // Finished typing the phrase, pause before starting to delete
+        typingDelay = delayAfterPhrase;
+        isDeleting = true;
+    } else if (isDeleting && currentCharIndex === 0) {
+        // Finished deleting, pause before starting to type the next phrase
+        typingDelay = delayAfterDeleting;
+        isDeleting = false;
+        currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+    }
+
+    setTimeout(type, typingDelay);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(type, 1000);
+});
